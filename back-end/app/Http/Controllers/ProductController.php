beb06430ceacea79cd85d83005c466c2f3c33ba1
Jerico Purnomo <jericoashpurnomo100@gmail.com>
2025-10-03 08:56:58 +0700

    Backend category and product controller + API done

--- back-end/app/Http/Controllers/ProductController.php
+++ back-end/app/Http/Controllers/ProductController.php
@@ -1,15 +1,24 @@
 <?php
 
 
+
+
 namespace App\Http\Controllers;
+
+
 
 
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 
+
+
 class ProductController extends Controller
 {
+    /**
+     * Menyimpan produk baru.
+     */
     public function store(Request $request)
     {
         $request->validate([
@@ -21,10 +30,16 @@ class ProductController extends Controller
         ]);
 
 
+
+
         $imageName = time().'.'.$request->image->extension();
 
 
+
+
         $request->image->storeAs('public/products', $imageName);
+
+
 
 
         $product = Product::create([
@@ -32,10 +47,73 @@ class ProductController extends Controller
             'price' => $request->price,
             'stock' => $request->stock,
             'category_id' => $request->category_id,
-            'image' => $imageName, 
+            'image' => $imageName,
         ]);
 
 
+
+
         return response()->json($product, 201);
+    }
+
+
+    /**
+     * Memberikan 5 rekomendasi produk acak saat search bar diklik.
+     */
+    public function recommendations()
+    {
+        $products = Product::where('is_active', true)->inRandomOrder()->limit(5)->get();
+        return response()->json($products);
+    }
+
+
+    /**
+     * Fitur live search, memberikan 5 produk sesuai input.
+     */
+    public function searchSuggestions(Request $request)
+    {
+        $query = $request->input('q');
+
+
+        if (empty($query)) {
+            return $this->recommendations();
+        }
+
+
+        $products = Product::where('is_active', true)
+                            ->where('name', 'LIKE', "%{$query}%")
+                            ->orWhereHas('category', function ($q) use ($query) {
+                                $q->where('name', 'LIKE', "%{$query}%");
+                            })
+                            ->limit(5)
+                            ->get();
+
+
+        return response()->json($products);
+    }
+
+
+    /**
+     * Pencarian penuh saat pengguna menekan Enter.
+     */
+    public function search(Request $request)
+    {
+        $query = $request->input('q');
+
+
+        if (empty($query)) {
+            return response()->json([]);
+        }
+
+
+        $products = Product::where('is_active', true)
+                           ->where('name', 'LIKE', "%{$query}%")
+                           ->orWhereHas('category', function ($q) use ($query) {
+                               $q->where('name', 'LIKE', "%{$query}%");
+                           })
+                           ->get();
+
+
+        return response()->json($products);
     }
 }
